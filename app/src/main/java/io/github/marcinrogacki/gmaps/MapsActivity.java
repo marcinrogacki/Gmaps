236bfe2855189703dbe01116ac60a4aefa620b25
Marcin Rogacki <mrogacki@nexway.com>
2017-08-11 19:54:47 +0200

    Init Android project with Google Maps and Gradle
    new file:   .gitignore
    new file:   .idea/compiler.xml
    new file:   .idea/copyright/profiles_settings.xml
    new file:   .idea/gradle.xml
    new file:   .idea/misc.xml
    new file:   .idea/modules.xml
    new file:   .idea/runConfigurations.xml
    new file:   app/.gitignore
    new file:   app/build.gradle
    new file:   app/proguard-rules.pro
    new file:   app/src/androidTest/java/io/github/marcinrogacki/gmaps/ExampleInstrumentedTest.java
    new file:   app/src/debug/res/values/google_maps_api.xml
    new file:   app/src/main/AndroidManifest.xml
    new file:   app/src/main/java/io/github/marcinrogacki/gmaps/MapsActivity.java
    new file:   app/src/main/java/io/github/marcinrogacki/gmaps/MyLocationListener.java
    new file:   app/src/main/res/layout/activity_maps.xml
    new file:   app/src/main/res/mipmap-hdpi/ic_launcher.png
    new file:   app/src/main/res/mipmap-hdpi/ic_launcher_round.png
    new file:   app/src/main/res/mipmap-mdpi/ic_launcher.png
    new file:   app/src/main/res/mipmap-mdpi/ic_launcher_round.png
    new file:   app/src/main/res/mipmap-xhdpi/ic_launcher.png
    new file:   app/src/main/res/mipmap-xhdpi/ic_launcher_round.png
    new file:   app/src/main/res/mipmap-xxhdpi/ic_launcher.png
    new file:   app/src/main/res/mipmap-xxhdpi/ic_launcher_round.png
    new file:   app/src/main/res/mipmap-xxxhdpi/ic_launcher.png
    new file:   app/src/main/res/mipmap-xxxhdpi/ic_launcher_round.png
    new file:   app/src/main/res/values/colors.xml
    new file:   app/src/main/res/values/strings.xml
    new file:   app/src/main/res/values/styles.xml
    new file:   app/src/release/res/values/google_maps_api.xml
    new file:   app/src/test/java/io/github/marcinrogacki/gmaps/ExampleUnitTest.java
    new file:   build.gradle
    new file:   gradle.properties
    new file:   gradle/wrapper/gradle-wrapper.jar
    new file:   gradle/wrapper/gradle-wrapper.properties
    new file:   gradlew
    new file:   gradlew.bat
    new file:   settings.gradle

---
+++ app/src/main/java/io/github/marcinrogacki/gmaps/MapsActivity.java
@@ -0,0 +1,70 @@
+package io.github.marcinrogacki.gmaps;
+
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import android.location.LocationManager;
+import android.location.LocationListener;
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.content.Context;
+
+import io.github.marcinrogacki.gmaps.MyLocationListener;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private LocationListener locationListener;
+    private LocationManager locationManager;
+
+    private static final String[] INITIAL_PERMS={
+        Manifest.permission.ACCESS_FINE_LOCATION
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+        locationManager = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
+
+    }
+
+    private boolean canAccessLocation() {
+        return(hasPermission(Manifest.permission.ACCESS_FINE_LOCATION));
+    }
+
+    private boolean hasPermission(String perm) {
+        return(PackageManager.PERMISSION_GRANTED==checkSelfPermission(perm));
+    }
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        if (!canAccessLocation()) {
+            requestPermissions(INITIAL_PERMS, 1337);
+        } else {
+            locationListener = new MyLocationListener(googleMap);
+            locationManager.requestLocationUpdates(
+                LocationManager.GPS_PROVIDER, 0, 0, locationListener
+            );
+        }
+    }
+}
---
+++ app/src/main/java/io/github/marcinrogacki/gmaps/MyLocationListener.java
@@ -0,0 +1,47 @@
+package io.github.marcinrogacki.gmaps;
+
+import android.location.LocationListener;
+import android.os.Bundle;
+import android.location.Location;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.CameraUpdateFactory;
+
+
+/*---------- Listener class to get coordinates ------------- */
+class MyLocationListener implements LocationListener {
+
+    private GoogleMap mMap;
+    private MarkerOptions player;
+
+    public MyLocationListener(GoogleMap m)
+    {
+        mMap = m;
+        player = new MarkerOptions();
+        player.title("You");
+    }
+
+    @Override
+    public void onLocationChanged(Location loc) {
+//        if (player == null) {
+            player.position(new LatLng(loc.getLatitude(), loc.getLongitude()));
+            mMap.clear();
+            mMap.addMarker(player);
+            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(player.getPosition(), 20));
+            //mMap.moveCamera(CameraUpdateFactory.newLatLng(player));
+//        } else {
+//            player = new LatLng(loc.getLatitude(), loc.getLongitude());
+//            mMap.moveCamera(CameraUpdateFactory.newLatLng(player));
+//        }
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {}
+
+    @Override
+    public void onProviderEnabled(String provider) {}
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {}
+}
